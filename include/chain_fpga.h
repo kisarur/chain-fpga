0bc3ba5fd8569b6afd7fc7d8277d098a73ca724c
Kisaru Liyanage <kisarur@gmail.com>
2023-02-08 22:12:49 +1100

    update comments

--- include/chain_fpga.h
+++ include/chain_fpga.h
@@ -13,7 +13,7 @@
 
 using namespace std;
 
-// Parameters of the hardware architecture (Important: don't change the values below unless you recompile hardware code (device/minimap2_opencl.cl))
+// Parameters of the hardware architecture (Important: don't change the values below unless you recompile hardware code (src/fpga/minimap2_opencl.cl))
 #define NUM_HW_KERNELS 1
 #define TRIPCOUNT_PER_SUBPART 64
 #define MAX_SUBPARTS 8
@@ -22,7 +22,7 @@ using namespace std;
 #define EXTRA_ELEMS 0  // added to temporarily fix the issue with parallel execution of OpenCL hardware kernels on Intel devices
                        // (i.e. all input/output arrays used in hardware chaining are filled with EXTRA_ELEMS no. of elements)
 
-// #define DEBUG_HW          // chain_hardware.cpp (to print out steps in hardware processing)
+// #define DEBUG_HW    // to print out steps in hardware processing
 
 // Size of the OpenCL buffers used for data transfer
 #define DEVICE_MAX_N 332000000
